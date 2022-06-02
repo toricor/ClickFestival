d47d22d95224d5d5eb2553fa57e3c6c4b537f831
toricor <toriyabe0821@gmail.com>
2022-06-02 13:57:24 +0900

    fix androidTest

--- app/src/androidTest/java/com/github/toricor/clickfestival/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/github/toricor/clickfestival/ExampleInstrumentedTest.kt
@@ -20,6 +20,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.github.toricor.clickfestival", appContext.packageName)
+        assertEquals("com.github.toricor.clickfestival.dev", appContext.packageName)
     }
 }
