9bb6c2105607e88c532b836fbfcfeef165063d09
toricor <toriyabe@mfac.jp>
2022-06-02 10:42:10 +0900

    change packages

--- app/src/main/java/com/github/toricor/clickfestival/FirstFragment.kt
+++ app/src/main/java/com/github/toricor/clickfestival/FirstFragment.kt
@@ -1,4 +1,4 @@
-package com.example.clickfestival
+package com.github.toricor.clickfestival
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
@@ -6,6 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.fragment.findNavController
+import com.example.clickfestival.R
 import com.example.clickfestival.databinding.FragmentFirstBinding
 
 /**
--- app/src/main/java/com/github/toricor/clickfestival/MainActivity.kt
+++ app/src/main/java/com/github/toricor/clickfestival/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.clickfestival
+package com.github.toricor.clickfestival
 
 import android.os.Bundle
 import com.google.android.material.snackbar.Snackbar
@@ -9,6 +9,7 @@ import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
 import android.view.Menu
 import android.view.MenuItem
+import com.example.clickfestival.R
 import com.example.clickfestival.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
