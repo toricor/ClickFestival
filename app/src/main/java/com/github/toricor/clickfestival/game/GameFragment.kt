a303b9c5fc4549b4995447e945af867c9c3d84f7
toricor <toriyabe0821@gmail.com>
2022-06-02 13:18:14 +0900

    add some click effects

--- app/src/main/java/com/github/toricor/clickfestival/game/GameFragment.kt
+++ app/src/main/java/com/github/toricor/clickfestival/game/GameFragment.kt
@@ -1,10 +1,14 @@
 package com.github.toricor.clickfestival.game
 
+import android.os.Build
 import android.os.Bundle
+import android.os.VibrationEffect
+import android.os.Vibrator
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.getSystemService
 import androidx.lifecycle.LifecycleOwner
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
@@ -33,21 +37,38 @@ class GameFragment : Fragment() {
         binding.gameViewModel = viewModel
         binding.lifecycleOwner = this
 
-        return binding.root
-    }
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-
         binding.buttonCountUp.setOnClickListener {
             if (findNavController().currentDestination?.id == R.id.GameFragment) {
                 findNavController().navigate(R.id.action_GameFragment_to_TopFragment)
             }
         }
+
+        // Buzzes
+        viewModel.eventBuzz.observe(viewLifecycleOwner, Observer { buzzType ->
+            if (buzzType != GameViewModel.BuzzType.NO_BUZZ) {
+                buzz(buzzType.pattern)
+                viewModel.onBuzzComplete()
+            }
+        })
+
+        return binding.root
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    private fun buzz(pattern: LongArray) {
+        val buzzer = activity?.getSystemService<Vibrator>()
+
+        buzzer?.let {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                buzzer.vibrate(VibrationEffect.createWaveform(pattern, -1))
+            } else {
+                //deprecated in API 26
+                buzzer.vibrate(pattern, -1)
+            }
+        }
     }
 }
--- app/src/main/java/com/github/toricor/clickfestival/game/GameViewModel.kt
+++ app/src/main/java/com/github/toricor/clickfestival/game/GameViewModel.kt
@@ -3,13 +3,70 @@ package com.github.toricor.clickfestival.game
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import kotlin.math.min
+
+private val CLICK_BUZZ_PATTERN = longArrayOf(100, 100)
+private val GAME_OVER_BUZZ_PATTERN = longArrayOf(0, 2000)
+private val NO_BUZZ_PATTERN = longArrayOf(0)
+private val INITIAL_TEXT_SIZE = 72
+private val MAX_TEXT_SIZE = 72
 
 class GameViewModel: ViewModel() {
+
+    enum class BuzzType(val pattern: LongArray) {
+        CLICK(CLICK_BUZZ_PATTERN),
+        GAME_OVER(GAME_OVER_BUZZ_PATTERN),
+        NO_BUZZ(NO_BUZZ_PATTERN)
+    }
+
     private val _clickCount = MutableLiveData<Int>(0)
     val clickCount: LiveData<Int>
         get() = _clickCount
 
+    private val _textSize = MutableLiveData<Int>(INITIAL_TEXT_SIZE)
+    val textSize: LiveData<Int>
+        get() = _textSize
+
+    private val _textRotation = MutableLiveData<Int>()
+    val textRotation: LiveData<Int>
+        get() = _textRotation
+
+    private val _eventGameFinish = MutableLiveData<Boolean>(false)
+
+    private val _eventBuzz = MutableLiveData<BuzzType>(BuzzType.NO_BUZZ)
+    val eventBuzz: LiveData<BuzzType>
+        get() = _eventBuzz
+
     fun onButtonClick() {
         _clickCount.value = _clickCount.value?.plus(1)
+
+        val clickCount = clickCount.value!!
+
+        // some effects that children enjoy
+        _eventBuzz.value = BuzzType.CLICK
+
+        if (clickCount < 24) {
+            return
+        }
+
+        if (24 < clickCount) {
+            _textSize.value = calcTextSize(clickCount)
+        }
+
+        if (60 < clickCount) {
+            _textRotation.value = calcTextRotation(clickCount)
+        }
+    }
+
+    private fun calcTextRotation(v: Int): Int {
+        return (v - 60) * 5
+    }
+
+    private fun calcTextSize(v: Int): Int {
+        return v * 5
+    }
+
+    fun onBuzzComplete() {
+        _eventBuzz.value = BuzzType.NO_BUZZ
     }
 }
